f81710aaae35295b391feda51a931d05423c0627
qkqkq <qkqkq@SAN>
2025-03-11 21:44:04 +0900

    3/11 과제수행

--- HelloWorld/src/com/yedam/variable/VarExe6.java
+++ HelloWorld/src/com/yedam/variable/VarExe6.java
@@ -25,22 +25,22 @@ public class VarExe6 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		
-//		Member m1 = new Member(); //인스턴스 생성.
-//		m1.name = "홍길동";
-//		System.out.println(m1.score);
-//		Member m2 = new Member(); //인스턴스 생성.
-//		m2.name = "최민수";
-//		Member m3 = new Member(); //인스턴스 생성.
-//		m3.name = "김병수";
-//		Member m4 = new Member(); //인스턴스 생성.
-//		m4.name = "박인만";
-//		
+		Member m1 = new Member(); //인스턴스 생성.
+		m1.setMember("홍길동", 0);
+		Member m2 = new Member(); //인스턴스 생성.
+		m2.setMember("최민수", 0);
+		Member m3 = new Member(); //인스턴스 생성.
+		m3.setMember("김병수", 0);
+		Member m4 = new Member(); //인스턴스 생성.
+		m4.setMember("박인만", 0);
+		
 		//배열.
-//		Member[] members = {m1, m2, m3, m4};
-		// 70에서 100 사이의 임의의 값으로 점수 지정.
-//		for (int i = 0; i< members.length; i++) {
-//			members[i].score = (int) (Math.random() * 31 ) + 70;
-//		}
+		Member[] members = {m1, m2, m3, m4};
+		 //70에서 100 사이의 임의의 값으로 점수 지정.
+		for (int i = 0; i< members.length; i++) {
+			int randomScore = (int) (Math.random() * 31 ) + 70;
+			members[i].setScore(randomScore);
+		}
 		
 		//조회 이름을 입력 -> 점수 출력.
 		System.out.println("조회할 이름 입력>> ");
@@ -49,26 +49,26 @@ public class VarExe6 {
 		//for 반복문 활용. 
 		//30 == 30, members[i].name.equals(search)
 		
-//		for (int i=0; i<members.length; i++) {
-//			if(members[i].getName().equals(search)) {
-//				System.out.println(members[i].getScore());
-//			}
-//		}
+		for (int i=0; i<members.length; i++) {
+			if(members[i].getName().equals(search)) {
+				System.out.println(members[i].getScore());
+			}
+		}
 		
 		
 		
 		
 		// 점수가 가장 높은 사람의 이름 출력
-//		int max = members[0].score;
-//		String name = members[0].name;
-//		for (int i = 0; i < members.length; i++) {
-//			System.out.println(members[i].name+","+members[i].score);
-//			if(max < members[i].score) {
-//				max = members[i].score;
-//				name = members[i].name;
-//			}
-//		}
-//		System.out.println("최고점수는 " + name);
+		int max = members[0].getScore();
+		String name = members[0].getName();
+		for (int i = 0; i < members.length; i++) {
+			System.out.println(members[i].getName()+","+members[i].getScore());
+			if(max < members[i].getScore()) {
+				max = members[i].getScore();
+				name = members[i].getName();
+			}
+		}
+		System.out.println("최고점수는 " + name);
 
 
 	}//end of main().
